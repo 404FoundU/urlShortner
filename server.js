4c0a771dc4a22662ea421017ceb0e288fbce46a2
upillai <upillai@xpressdocs.com>
2021-05-03 16:40:32 -0500

    Added server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+dotenv.config();
+const app = express();
+const port = 5001;
+app.use(cors());
+mongoose.connect(
+    process.env.DB_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    }
+)
+    .catch(e => {
+        console.error(e);
+        process.exit(1);
+    })
+    .then(async client => {
+        app.listen(port, () => {
+            console.log(`Listening on port ${port}`);
+        });
+    });
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}))
+app.get('/', async (req, res) => {
+    res.send('Hello');
+});
+
+
