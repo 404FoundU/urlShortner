67940b25289d743630bd4d694eccd2f028319997
upillai <upillai@xpressdocs.com>
2021-05-04 10:37:22 -0500

    added clicks

--- routes/routeer.js
+++ routes/routeer.js
@@ -9,17 +9,25 @@ router.get('/', async (req, res) => {
     const shorts = await shortUrl.find();
     res.render('index', {shorts})
 });
-router.post('/shortUrls', async (req, res)=>{
+router.post('/shortUrls', async (req, res) => {
     const short = new shortUrl();
     short.full = req.body.fullUrl;
     try {
         await short.save();
         // res.send(short);
         res.redirect('/')
-    }
-    catch (e){
+    } catch (e) {
         console.error(e);
         res.status(500).send(e);
     }
+});
+router.get('/:shortUrl', async (req,res) =>{
+    const shorts = await shortUrl.findOne({short: req.params.shortUrl})
+    if (shorts == null) {
+        res.send(404);
+    }
+    shorts.clicks++;
+    await  shorts.save();
+    res.redirect(shorts.full);
 })
 export default router;
